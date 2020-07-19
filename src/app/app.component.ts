4f86d75dca5b41327ca65475a72d361b249051f2
Joster422 <Joster422@gmail.com>
2020-07-18 21:18:26 -0400

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent { }
