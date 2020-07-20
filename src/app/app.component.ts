cd4fb53795808cde470f6b114997da0179b49bcc
Joster422 <Joster422@gmail.com>
2020-07-19 21:01:38 -0400

    hero and cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-
+import { cards } from './cards.const';
 @Component({
   selector: 'jo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  cards = cards;
+  searchTerm: string | null = null;
+}
---
+++ src/app/cards.const.ts
@@ -0,0 +1,56 @@
+import { Card } from './card.interface';
+
+export const cards: Card[] = [
+  {
+    title: 'Icons 🌀',
+    link: 'https://joster-dev.github.io/icon/',
+    tags: [
+      'angular library 📚',
+      'npm package 📦'
+    ]
+  },
+  {
+    title: 'Form Controls 📈',
+    link: 'https://joster-dev.github.io/form-control/',
+    tags: [
+      'angular library 📚',
+      'npm package 📦'
+    ]
+  },
+  {
+    title: 'Checkers',
+    link: 'https://joster-dev.github.io/checkers/',
+    tags: [
+      'coming soon 🔜'
+    ]
+  },
+  {
+    title: 'Tic Tac Toe 😆',
+    link: 'https://joster-dev.github.io/tic-tac-toe/',
+    tags: [
+      'abstract game 🎮',
+      'local two player 👥',
+      'bot player 🤖',
+      'mobile friendly 📱'
+    ]
+  },
+  {
+    title: 'Ogre 👹',
+    link: 'https://joster-dev.github.io/ogre/',
+    tags: [
+      'abstract game 🎮',
+      'local two player 👥',
+      'bot player 🤖',
+      'mobile friendly 📱'
+    ]
+  },
+  {
+    title: 'Mine Sweeper 💣',
+    link: 'https://joster-dev.github.io/mine-sweeper/',
+    tags: [
+      'abstract game 🎮',
+      'single player 👤',
+      'ghost player 👻'
+    ]
+  }
+];
